fc0a98e12d2c826f1148d203af5c8df620e5db4a
Anton Cristo <antoncristo92@gmail.com>
2021-12-16 10:15:53 +0200

    mongoose connection

--- index.ts
+++ index.ts
@@ -8,27 +8,35 @@ import { env } from "./src/config";
 config();
 const mongoDbconnectionString = `mongodb+srv://free-user:${process.env.MONGO_PASSWORD}@freecluster.tomz5.mongodb.net/taskinDB?retryWrites=true&w=majority`;
 
-(async function main() {
-  try {
-    const app: express.Application = new Server().app;
-    const server: HttpServer = createServer(app);
+// (async function main() {
+//   try {
 
-    await mongoose.connect(mongoDbconnectionString);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
 
+const app: express.Application = new Server().app;
+const server: HttpServer = createServer(app);
+
+mongoose
+  .connect(mongoDbconnectionString)
+  .then(() => {
     console.log("mongoDB connected");
-
+  })
+  .then(() => {
     server.listen(env.NODE_PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-    server.on("listening", () => {
-      console.log(
-        `Server is listening on port ${env.NODE_PORT}, environment=${env.NODE_ENV}`
-      );
-    });
+server.on("listening", () => {
+  console.log(
+    `Server is listening on port ${env.NODE_PORT}, environment=${env.NODE_ENV}`
+  );
+});
 
-    server.on("close", () => {
-      console.log("Express server finished");
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})();
+server.on("close", () => {
+  console.log("Express server finished");
+});
